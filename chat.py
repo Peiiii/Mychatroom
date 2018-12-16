53de12c4f76437fed25f2e93b679a4c89b25e8ee
unknown <1535376447@qq.com>
2018-12-16 23:21:05 +0800

    change the html

--- chat.py
+++ chat.py
@@ -40,8 +40,9 @@ def handle_say_hi(msg):
 def log(event):
     print('-->>>{}:  '.format(event),end='')
 if __name__=="__main__":
-    addr=('127.0.0.1',80)
-    print('Serve on http://{}:{}'.format(addr[0],addr[1]))
+    addr=('0.0.0.0',80)
+    print('http://127.0.0.1:80...')
+    #print('Serve on http://{}:{}'.format(addr[0],addr[1]))
     socketio.run(app,host=addr[0],port=addr[1])
 
 
